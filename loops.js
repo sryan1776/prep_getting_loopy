8ff57d0d2e504b4a5d8874474d3f317018e42f58
Shawna Ryan <Shawna@silverfox.local>
2016-05-10 21:23:56 -1000

    complete president exercise

--- loops.js
+++ loops.js
@@ -7,3 +7,12 @@ var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
 		console.log("The value of i is: " + i);
 		console.log("The value at Index is: " + presidents[i]);
 	}
+var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
+
+	function printContent(n1, n2) {
+		for (var i=0; i<presidents.length; i++){
+		console.log(presidents[i]);
+		console.log(n1("The value of i is: " + i));
+		console.log(n2 ("The value at Index is: " + presidents[i]));
+	}
+}
