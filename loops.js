d0749ebb5e48912db4cb14c2016adf4a489563dd
Shawna Ryan <Shawna@silverfox.local>
2016-05-10 21:01:04 -1000

    first commit

--- loops.js
+++ loops.js
@@ -1,1 +1,9 @@
 // Do your work here
+
+var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
+
+	for (var i=0; i<presidents.length; i++){
+		console.log(presidents[i]);
+		console.log("The value of i is: " + i);
+		console.log("The value at Index is: " + presidents[i]);
+	}
